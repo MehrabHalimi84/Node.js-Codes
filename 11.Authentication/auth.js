967f583f08e34cc95cdbc33dbbe692b1fa2be381
Mehrab Halimi <shekarbaba48@gmail.com>
2025-07-31 03:58:33 +0330

    Json Web Token (jwt)

---
+++ 11.Authentication/auth.js
@@ -0,0 +1,67 @@
+const Joi = require('joi');
+const jwt = required('jsonwebtoken');
+const _ = require('lodash');
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/form')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.log(`Could not connect to MongoDB...  ,${err}`));
+
+const User = mongoose.model('User', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
+}));
+
+
+
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(1024).required()
+    };
+
+    return Joi.object(schema).validate(req);
+};
+
+
+
+app.post('/api/auth', async (req, res) => {
+    const result = validate(req.body)
+    if (result.error) return res.status(400).send(result.error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Invalid email or password.');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid email or password.')
+
+    const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey')
+
+    res.send(token);
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));

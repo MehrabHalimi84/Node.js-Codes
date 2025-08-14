164db8d5d49de5cf8996e9769b4742b3d0aeb585
Mehrab Halimi <shekarbaba48@gmail.com>
2025-08-15 00:21:37 +0330

    Handling and Logging Errors

--- 12.ErrorHandeling/index.js
+++ 12.ErrorHandeling/index.js
@@ -1,3 +1,7 @@
+const winston = require('winston/lib/winston/config');
+
+require('winston-mongodb')
+
 function asyncMiddleware(handler) {
     return async (req, res, next) => {
         try {
@@ -13,3 +17,11 @@ app.get('/me', auth, asyncMiddleware(async (req, res) => {
     const user = await User.findById(req.user._id).select('-password');
     res.send(user);
 }));
+
+winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/ProjectName' });
+
+
+process.on('uncaughtException', (ex) => {
+    console.log('WE GOT AN UNCAUGHT EXCEPTION');
+    winston.error(ex.message, ex);
+});
